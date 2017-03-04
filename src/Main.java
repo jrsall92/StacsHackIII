fb4e15073d5849e99f7415ad5b582e538c2037b4
jrsall92 <jrsall92@gmail.com>
2017-03-04 15:00:30 +0000

    title

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,7 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("hi");
-        JFrame window = new JFrame("Hunt The Wumpus");
+        JFrame window = new JFrame("Return of the Arcades");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setContentPane(new Screen());
         window.pack();
--- src/Screen.java
+++ src/Screen.java
@@ -1,12 +1,5 @@
-import javax.imageio.ImageIO;
-import javax.swing.JPanel;
-import java.awt.*;
-import java.awt.Font;
-import java.awt.image.*;
+import javax.swing.*;
 import java.awt.event.*;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 
 /**
  * Created by irs6 on 04/03/17.
@@ -31,23 +24,4 @@ public class Screen extends JPanel implements Runnable, KeyListener {
     public void run() {
 
     }
-
-    /*public static final int SCREEN_WIDTH = 600;
-    public static final int SCREEN_HEIGHT = 700;
-
-    @Override public void keyTyped(KeyEvent e) {
-
-        if(e.getKeyCode() == KeyEvent.VK_LEFT){
-            System.out.println("yo");
-        }
-
-    }
-
-    @Override public void keyPressed(KeyEvent e) {
-
-    }
-
-    @Override public void keyReleased(KeyEvent e) {
-
-    }*/
 }
