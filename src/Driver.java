a69f67a58c40cbb5ce9dcd5c1e40e1f8801e144f
jrsall92 <jrsall92@gmail.com>
2017-03-04 21:12:43 +0000

    added animation and getters in player class

--- src/Driver.java
+++ src/Driver.java
@@ -14,23 +14,23 @@ public class Driver extends PApplet {
 
     private boolean moveLeft, moveRight, moveUp, moveDown;
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         PApplet.main("Driver");
     }
 
     private StarP[] stars = new StarP[NUMBER_OF_STARS];
     private Comet[] comets = new Comet[NUMBER_OF_COMETS];
 
-    public void settings(){
+    public void settings() {
         size(SCREEN_WIDTH, SCREEN_HEIGHT);
     }
 
-    public void setup(){
-        for(int i = 0; i< NUMBER_OF_STARS; i++){
+    public void setup() {
+        for (int i = 0; i < NUMBER_OF_STARS; i++) {
             stars[i] = new StarP(this);
         }
 
-        for(int i = 0; i < NUMBER_OF_COMETS; i++){
+        for (int i = 0; i < NUMBER_OF_COMETS; i++) {
             comets[i] = new Comet(this);
         }
         moveLeft = false;
@@ -41,67 +41,68 @@ public class Driver extends PApplet {
         pl = new Player(this);
     }
 
-    public void draw(){
+    public void draw() {
         background(0, 0, 0);
         for (int i = 0; i < NUMBER_OF_STARS; i++) {
             stars[i].move();
             stars[i].show();
         }
-        for(int i = 0; i < NUMBER_OF_COMETS; i++){
+        for (int i = 0; i < NUMBER_OF_COMETS; i++) {
             comets[i].move();
             comets[i].show();
         }
-
 
         pl.show();
 
     }
 
-    public void keyPressed(){
+    public void keyPressed() {
 
+        if (key == CODED) {
+            //if(keyCode == UP){
+            //    moveUp = true;
+            //    //TODO move player up
+            //}
+            //else {
+            //    moveUp = false;
+            //}
+            //
+            //if(keyCode == DOWN){
+            //    moveDown = true;
+            //    //TODO move player down
+            //}
+            //else{
+            //    moveDown = false;
+            //}
 
-        if(key == CODED){
-            if(keyCode == UP){
-                moveUp = true;
-                //TODO move player up
-            }
-            else {
-                moveUp = false;
-            }
+            if (keyCode == RIGHT || keyCode == LEFT) {
+                if (keyCode == RIGHT) {
+                    moveRight = true;
+                    pl.move(20);
+                    //TODO move right
+                }
+                else {
+                    moveRight = false;
+                }
 
-            if(keyCode == DOWN){
-                moveDown = true;
-                //TODO move player down
-            }
-            else{
-                moveDown = false;
-            }
-
-            if(keyCode == RIGHT){
-                moveRight = true;
-                pl.move(20);
-                //TODO move right
-            }
-            else{
-                moveRight = false;
-            }
-
-            if(keyCode == LEFT){
-                moveLeft = true;
-                pl.move(-20);
-                //TODO move right
-            }
-            else{
-                moveLeft = false;
+                if (keyCode == LEFT) {
+                    moveLeft = true;
+                    pl.move(-20);
+                    //TODO move right
+                }
+                else {
+                    moveLeft = false;
+                }
             }
         }
-
-
-        else{
-            if(key == ' '){
-                System.out.println("Space");
-                comets[3].hit();
-            }
+        else if (key == ' ') {
+            System.out.println("Space");
+            comets[3].hit();
         }
+
+    }
+
+    public void keyReleased(){
+        pl.resetImage();
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,5 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 /**
  * Created by va29 on 04/03/17.
@@ -6,30 +7,70 @@ import processing.core.PApplet;
 
 public class Player {
 
+    private final int LEFT_TURN = 1;
+    private final int STRAIGHT = 2;
+    private final int RIGHT_TURN = 3;
+
     PApplet parent;
-    float x=290;
-    float y=840;
+    private float x=290;
+    private float y=840;
+    private PImage image;
+    private int imgNum;
+    private int width, height;
 
     public Player(PApplet p){
         parent = p;
         x  = 290;
-        y  = 840;
+        setImage(STRAIGHT);
+        y  = parent.height - height;
     }
 
     void show() {
-        parent.stroke(300, 300, 600);
-        parent.rect(x, y, 60, 60);
+        parent.image(image, x, y);
+    }
+
+    private void setImage(int n){
+        if(n<1 || n>3){
+            n = 2;
+        }
+        image = parent.loadImage("aircraft"+ n +".png");
+        width = image.width;
+        height = image.height;
+    }
+
+    public void resetImage(){
+        setImage(STRAIGHT);
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
     }
 
     void move(int m){
         x=x+m;
+
+        if(m>=0)
+            setImage(RIGHT_TURN);
+        else
+            setImage(LEFT_TURN);
 
         if(x>parent.width)
             x = 1;
 
         if(x <=0 )
             x = parent.width -1;
-        System.out.println("aaaaaaaaaaaaa");
     }
 
 }
